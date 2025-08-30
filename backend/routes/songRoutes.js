cc7852680c6fa3acdb0c7ad1595ad0b43799ddc7
Issac038 <issac.antony@kalvium.community>
2025-08-30 09:29:57 +0530

    finished dynamic prompting

--- backend/routes/songRoutes.js
+++ backend/routes/songRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 // ✅ Correct import (with destructuring)
-const { generateSongRecommendation, oneShot, multiShot } = require("../controllers/songController");
+const { generateSongRecommendation, oneShot, multiShot,dynamicPrompt } = require("../controllers/songController");
 
 // ✅ Register route
 router.post("/recommend", generateSongRecommendation);
 router.get("/oneShot", oneShot);
 router.get("/multiShot", multiShot);
+router.get("/dynamicPrompt", dynamicPrompt);
 
 module.exports = router;
